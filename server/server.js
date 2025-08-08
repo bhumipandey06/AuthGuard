690937d2fb38e0cd0012c461dc89914077ecd03d
bhumipandey06 <bhumipandey1188@gmail.com>
2025-08-08 13:11:27 +0530

    Fix: updated CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 const allowedOrigins = ['http://localhost:5173',//localdev
-  'https://auth-guard-three.vercel.app/' //vercel frontend
+  'https://auth-guard-three.vercel.app' //vercel frontend
 ]
 
 app.use(express.json());
